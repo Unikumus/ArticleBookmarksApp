92c2df20613075693d025f6aee7de9b2b1054322
Unikumus <41722559+Unikumus@users.noreply.github.com>
2020-06-09 15:04:57 +0300

    Updates

---
+++ precache-manifest.cd066f0add71bc77dd579b5b6dccc539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a934e2d95ab62c99e103bc266b3ce4b",
+    "url": "/ArticleBookmarksApp/index.html"
+  },
+  {
+    "revision": "a423e3328fdc3d5585f1",
+    "url": "/ArticleBookmarksApp/static/css/main.7f11355f.chunk.css"
+  },
+  {
+    "revision": "06f9340f5d79fdc0de9d",
+    "url": "/ArticleBookmarksApp/static/js/2.49375d86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ArticleBookmarksApp/static/js/2.49375d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a423e3328fdc3d5585f1",
+    "url": "/ArticleBookmarksApp/static/js/main.ffecf0f9.chunk.js"
+  },
+  {
+    "revision": "accfd03b007648fdb951",
+    "url": "/ArticleBookmarksApp/static/js/runtime-main.7e287013.js"
+  }
+]);
